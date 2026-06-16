8ccdf529eaac66ca4e5563784722e93650badc80
YeniferAponte <117775275+YeniferAponte@users.noreply.github.com>
2026-06-16 14:36:25 -0400

    Change script shebang and improve error handling

--- test.sh
+++ test.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Loading AWS credentials from mounted file..."
+echo "Loading AWS credentials from /tmp/aws.env ..."
 
-export $(cat /aws.env | xargs)
+if [ -f /tmp/aws.env ]; then
+  set -a
+  . /tmp/aws.env
+  set +a
+else
+  echo "ERROR: aws.env not found!"
+  exit 1
+fi
 
-echo "DEBUG:"
-echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
 echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION"
 
 pip install awscli >/dev/null 2>&1 || true
 
 echo "Checking AWS identity..."
 aws sts get-caller-identity
+``
