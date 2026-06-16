257fee433a3041a694b62a6a92eb590784b4d350
YeniferAponte <117775275+YeniferAponte@users.noreply.github.com>
2026-06-16 11:56:35 -0400

    Refactor AWS CLI installation script

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "Installing AWS CLI..."
+echo "Checking environment variables..."
 
-pip install awscli
+env | grep AWS
+
+echo "Exporting AWS variables..."
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 
 echo "Checking AWS identity..."
-
 aws sts get-caller-identity
-``
