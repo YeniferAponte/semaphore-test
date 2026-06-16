d795adaaf7f2d010c0463f3a23abdfb95e49dd3f
YeniferAponte <117775275+YeniferAponte@users.noreply.github.com>
2026-06-16 12:03:05 -0400

    Set default AWS region to us-east-1

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ echo "Exporting AWS variables..."
 
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+export AWS_DEFAULT_REGION=us-east-1
 
 echo "Checking AWS identity..."
 aws sts get-caller-identity
